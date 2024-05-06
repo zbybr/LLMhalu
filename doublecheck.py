943b1b3b888c67bf38e10ccfff589940c2dd21ac
Borui Yang <35630438+zbybr@users.noreply.github.com>
2024-05-07 00:37:41 +0800

    Update doublecheck.py

--- doublecheck.py
+++ doublecheck.py
@@ -4,17 +4,11 @@ import pandas as pd
 dataset_path = "comparison.csv"
 df = pd.read_csv(dataset_path, encoding='latin-1')
 
-total = 0
-true_s = 0
-true = 0
-false = 0
-false_s = 0
-higher = 0
-higher_s = 0
-lower = 0
-lower_s = 0
-check = 0
-check_s = 0
+total = 0 # total hallucination samples
+true_s = 0 # true positive from selfcheckgpt
+true = 0 # true positive
+check = 0 # TP+FP
+check_s = 0 # TP+FP
 
 threshold = 0.5
 
@@ -28,19 +22,6 @@ for _, row in df.iterrows():
             true_s += 1
         if score >= threshold:
             true += 1
-        if score_s <= score:
-            higher += 1
-        else:
-            higher_s += 1
-    else:
-        if score_s > threshold:
-            false_s += 1
-        if score > threshold:
-            false += 1
-        if score_s >= score:
-            lower += 1
-        else:
-            lower_s += 1
     if score >= threshold:
         check += 1
     if score_s >= threshold:
@@ -67,14 +48,3 @@ print("recall rate: ")
 print(str(true) + " / " + str(total))
 print(str(100.0 * true / total) + "\n")
 
-print("higher score: ")
-print(str(higher) + " / " + str(total))
-print(str(100.0 * higher / total) + "\n")
-print(str(higher_s) + " / " + str(total))
-print(str(100.0 * higher_s / total) + "\n")
-
-print("lower score: ")
-print(str(lower) + " / " + str((100 - total)))
-print(str(100.0 * lower / (100 - total)) + "\n")
-print(str(lower_s) + " / " + str((100 - total)))
-print(str(100.0 * lower_s / (100 - total)) + "\n")
