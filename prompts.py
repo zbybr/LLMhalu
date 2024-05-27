56f65c13c52b5f378799041b82fd720ec9e218ec
Borui Yang <35630438+zbybr@users.noreply.github.com>
2024-05-27 16:45:35 +0800

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -1,3 +1,8 @@
+SYSTEM_PROMPT = """For the question, please answer in 1 sentence including the question context. Please notice, 
+your answer must consider about the reality, not the myths, fairy tails or legends. If possible, do not include yes 
+or no at the beginning of the sentence."""
+
+
 MUTATED_QUESTION_GENERATION_PROMPT = """Generate {num_variation} variations of the given question using metamorphic testing technique.
 Ensure that the question is altered using different vocabulary while preserving its original meaning. One example is provided for you.
 Example Question: Was Johnny Depp born in Boston?
@@ -19,9 +24,12 @@ Answer: Baseball is the most popular sport in Japan.
 Mutations:
 1. Japan holds baseball as its most widely embraced sport.
 2. The sport with the highest popularity in Japan is baseball.
-3. Baseball reigns as Japan's most favored sport among the populace.
 Notice how the full context is included in the each generated synonyms.
 If you generated just 'baseball' it would not make a meaningful sentence.
+Also, if there is a saying sentence in the answer, you should not modify it.
+For example:
+Answer: Michael Cunningham wrote "You cannot find peace by avoiding life" in "The Hours".
+Mutation: "You cannot find peace by avoiding life" was wrote by Michael Cunningham.
 """
 
 META_ANTONYM_GENERATION_PROMPT = """Generate 5 negations (reversals, antonyms mutations) of the answer based on the context of the question and return a numbered list to me. 
@@ -33,9 +41,16 @@ Answer: Baseball is the most popular sport in Japan.
 Mutations:
 1. The most popular sport in Japan is not baseball.
 2. Baseball is not the most popular sport in Japan.
-3. Japan does not consider baseball as the most popular sport.
 Be careful about double negations which make the sentence semantically same to the provided one. The context of the question 
 is really important. Notice how the antonyms are meaningful sentences in the example. 
 Also, you should negate the meaning of the sentence based on the question.
+Please notice, if there is a saying sentence in the answer, you should not modify it.
+For example:
+Answer: Michael Cunningham wrote "You cannot find peace by avoiding life" in "The Hours".
+Mutation: Michael Cunningham did not write "You cannot find peace by avoiding life" in "The Hours"
 Do not add any information that's not in the provided in the answer nor asked by the question.
 """
+
+FACT_VERIFICATION_PROMPT = """For the sentence, you should check whether it is correct truth in reality or not. 
+Please notice, all truth in myths, fairy tails and legends will not be considered as reality. Answer YES or NO. If you 
+are NOT SURE, answer NOT SURE. Don't return anything else except YES, NO, or NOT SURE."""
