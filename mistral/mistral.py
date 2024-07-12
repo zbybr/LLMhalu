49dce259aec81eba7bbfed503d8110d85a05fcb5
Afif Al Mamun <afifanik@gmail.com>
2024-07-11 22:53:47 -0600

    Update mistral model

--- mistral/mistral.py
+++ mistral/mistral.py
@@ -1,13 +1,17 @@
-from transformers import AutoTokenizer, MistralForCausalLM
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
 class Mistral:
     def __init__(self, model_id="mistralai/Mistral-7B-v0.1", max_new_tokens=512):
         self.tokenizer = AutoTokenizer.from_pretrained(model_id)
-        self.model = MistralForCausalLM.from_pretrained(model_id)
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_id,
+            device_map="auto",
+        )
         self.max_new_tokens = max_new_tokens
 
-    def invoke(self, system_prompt, question):
+    def invoke(self, system_prompt, question, temperature=0.1):
         messages = [
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": question},
@@ -25,7 +29,7 @@ class Mistral:
             input_ids,
             max_new_tokens=self.max_new_tokens,
             do_sample=True,
-            temperature=0.5,
+            temperature=temperature,
         )
         response = outputs[0][input_ids.shape[-1] :]
 
